28040a2e1ade56ba8c721726e54245c9bb86eba0
puzz <taste@inbox.ru>
2022-11-18 22:26:04 +0300

    make greetings

---
+++ greet/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Напиши свое имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "света")
+{
+   Console.WriteLine("Ура! Привет, Пузз!");
+}
+else
+{
+    Console.Write("Привет, душнила - ");
+    Console.WriteLine(username);
+}
